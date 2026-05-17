91751873d0c51a3e5a73eaddd95a0e146b0dc5b4
hikarinavis-web <yuuki0311ab@gmail.com>
2026-05-17 09:09:36 +0900

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "光回線 営業管理ツール",
+  description: "営業管理Webアプリ",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ja">
+      <body>{children}</body>
+    </html>
+  );
+}
