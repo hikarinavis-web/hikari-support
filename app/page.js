64667ec25f759b6e63794e3a3d906ac92e613b9a
hikarinavis-web <yuuki0311ab@gmail.com>
2026-05-17 09:52:17 +0900

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,20 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>光回線 営業管理ツール</h1>
+
+      <input placeholder="名前" />
+      <br /><br />
+
+      <input placeholder="電話番号" />
+      <br /><br />
+
+      <input placeholder="住所" />
+      <br /><br />
+
+      <button>判定する</button>
+
+      <p>推定キャリア：docomo系</p>
+    </main>
+  );
+}
